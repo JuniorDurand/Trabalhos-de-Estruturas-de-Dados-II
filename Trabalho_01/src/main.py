f35fedbb1b63cd5e02e508ff56a83688c585124b
DURAND <junior_durand@outlook.com>
2019-09-28 22:51:53 -0300

    adicionando opções por linha de comando

--- Trabalho_01/src/main.py
+++ Trabalho_01/src/main.py
@@ -2,6 +2,8 @@ from grafo import Grafo
 from algoritmosDeOrdenacao import *
 from utils import *
 
+import argparse 
+import time
 
 
 
@@ -12,15 +14,92 @@ Instruções básicas de como fazer a implementação estão no arquivo algoritm
 
 if __name__ == "__main__":
 
-    #algoritimoDeOrdenacao = InsertionSort()
-    algoritimoDeOrdenacao = Quicksort()
-    arquivoJson = './grafos/7vertices.json'
-    arquivoDeSaida = './arvores_geradas/mst7Vertice2.txt'
 
-    grafo = Grafo()
-    grafo.estabelecerAlgoritmoDeOrdencao(algoritimoDeOrdenacao)
-    grafo.carregarGrafo(arquivoJson)
+	parser = argparse.ArgumentParser()
 
-    arvoreGeradoraMinima =  grafo.executarKruskal() 
-    SalvarArvoreGeradoraMinimaEmArquivo(arquivoDeSaida, arvoreGeradoraMinima)
+	#definindo algoritmo usado
+	parser.add_argument("algoritmo", type=str, help="Algoritmo de ordenacao")
+
+	#definindo arquivo de entrada
+	parser.add_argument("arquivo_entrada", type=str, help="Nome do arquivo de Entrada")
+
+	#definindo arquivo de saida
+	parser.add_argument("arquivo_saida", type=str, help="Nome do arquivo de Saída")
+
+	#definido tamanho L da partição por inserção
+	#argumento opcional deve ser utilizado apenas se o algoritmo selecionado
+	#suportar inserção parcial ou final
+	#deve ser digitado da seguinte forma:
+	#Em algoritmos que suportam inserção parcial:
+	# "main.py [algoritmo] [arquivo de entrada] [arquivo de saida] [-l  tamanho da partição por inserção]"
+
+	#Em algoritmos que  NÃO suportam inserção parcial:
+	# "main.py [algoritmo] [arquivo de entrada] [arquivo de saida]"
+	parser.add_argument('-l', '--length', type=int, help="tamanho da partição por inserção")
+
+	args = parser.parse_args()
+
+	algoritmo = args.algoritmo
+	length = args.length
+
+	if length:
+
+		if algoritmo.lower() == "mergesortpartialinsert":
+			algoritmoDeOrdenacao = MergeSortPartialInsert(length)
+
+		elif algoritmo.lower() == "mergesortfinalinsert":
+			algoritmoDeOrdenacao = MergeSortFinalInsert(length)
+
+		elif algoritmo.lower() == "quicksortpartialinsert":
+			algoritmoDeOrdenacao = QuickSortPartialInsert(length)
+
+		elif algoritmo.lower() == "quicksortfinalinsert":
+			algoritmoDeOrdenacao = QuicksortFinalInsert(length)
+
+		else:
+			print("O algoritmo selecionado não suporta inserção parcial ou final")
+			print("Algoritmos que suportão:")
+			print("\tMergeSortPartialInsert")
+			print("\tMergeSortFinalInsert")
+			print("\tQuickSortPartialInsert")
+			print("\tQuickSortFinalInsert")
+			exit()
+
+	else:
+
+		if algoritmo.lower() == "insertionsort":
+			algoritmoDeOrdenacao = InsertionSort()
+
+		elif algoritmo.lower() == "selectionsort":
+			algoritmoDeOrdenacao = SelectionSort()
+
+		elif algoritmo.lower() == "shellsort":
+			algoritmoDeOrdenacao = ShellSort()
+
+		elif algoritmo.lower() == "mergesort":
+			algoritmoDeOrdenacao = MergeSort()
+
+		elif algoritmo.lower() == "quicksort":
+			algoritmoDeOrdenacao = Quicksort()
+
+		else:
+			print("O algoritmo selecionado precisa do tamanho de partição para inserção")
+			print("Digite da seguinte forma:")
+			print("\tmain.py [algoritmo] [arquivo de entrada] [arquivo de saida] [-l  tamanho da partição por inserção]")
+			print("\n\nExemplo:")
+			print("\tmain.py QuicksortFinalInsert 7vertices.json out.txt -l 4")
+			exit()
+
+
+
+
+	arquivoJson = args.arquivo_entrada
+	arquivoDeSaida = args.arquivo_saida
+
+	grafo = Grafo()
+	grafo.estabelecerAlgoritmoDeOrdencao(algoritmoDeOrdenacao)
+	grafo.carregarGrafo(arquivoJson)
+
+	arvoreGeradoraMinima =  grafo.executarKruskal() 
+	SalvarArvoreGeradoraMinimaEmArquivo(arquivoDeSaida, arvoreGeradoraMinima)
 
